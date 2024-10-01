c49d506a67e538793280fd82deff26b0e2d4069f
stmata <108939392+stmata@users.noreply.github.com>
2024-10-01 15:45:57 -0400

    Added templates
    All the required templates have been added

---
+++ fastapi_app/main.py
@@ -0,0 +1,23 @@
+import sys, os
+# Add the root directory to the syspath
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.wsgi import WSGIMiddleware
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+# Create fastAPI app
+
+app = FastAPI()
+
+
+# Get the absolute path to the templates directory
+templates_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "templates"))
+# Get the absolute path to the static directory
+static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "static"))
+# Serve static files
+app.mount("/static", StaticFiles(directory=static_dir))
+# Set up Jinja2 template for rendering HTML files
+templates = Jinja2Templates(directory=templates_dir)
